36c24b7efdf832b154febe9d12b3718d43ad2d85
Archit Bali <129682827+architinit@users.noreply.github.com>
2024-08-01 06:43:26 +0530

    Add files via upload

---
+++ Leetcode Problems/problem121.cpp
@@ -0,0 +1,18 @@
+//problem 121 solution on leetcode
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> prices={7,1,5,3,6,4};
+    int min=prices[0];
+    int sum=0;
+    for(int i=1;i<prices.size();i++){
+        if(sum<(prices[i]-min)){
+            sum=prices[i]-min;
+        }
+        if(prices[i]<min){
+            min=prices[i];
+        }
+    }
+    cout<<sum;
+}
